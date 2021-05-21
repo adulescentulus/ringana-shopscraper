761aa2b9b929d36ddab357e9a09a93176ba28ef5
Andreas Groll <andreas.groll@gmail.com>
2021-05-22 00:32:55 +0200

    fix: use UTF-8 encoding for JSON responses

--- src/main/java/de/networkchallenge/ringana/shopscraper/web/configuration/SpringConfig.java
+++ src/main/java/de/networkchallenge/ringana/shopscraper/web/configuration/SpringConfig.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
 
 //@EnableAutoConfiguration
 //@ComponentScan("de.networkchallenge.ringana.shopscraper.web.configuration")
@@ -22,7 +25,10 @@ public class SpringConfig {
     // }
     @Bean
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters()
+                .add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+        return restTemplate;
     }
 
     @Bean
