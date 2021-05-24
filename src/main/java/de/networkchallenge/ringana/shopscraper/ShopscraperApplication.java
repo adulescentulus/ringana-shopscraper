8af0e4836ae8a40dc34100f434b2a81880cb6ace
Andreas Groll <andreas.groll@gmail.com>
2021-05-24 12:36:14 +0200

    test

--- src/main/java/de/networkchallenge/ringana/shopscraper/ShopscraperApplication.java
+++ src/main/java/de/networkchallenge/ringana/shopscraper/ShopscraperApplication.java
@@ -6,6 +6,8 @@ import org.springframework.nativex.hint.TypeHint;
 import org.springframework.nativex.hint.TypeHints;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Locale;
 
 @SpringBootApplication
 @TypeHints(value = {
@@ -18,6 +20,11 @@ public class ShopscraperApplication {
 	public static void main(String[] args) throws IOException {
 		System.out.write("Îƞŧéřƞȧŧǐøƞȧŀǐẑȧŧǐøƞ".getBytes());
 		System.out.println(java.util.TimeZone.getDefault().getID());
+		System.out.println("LANG=" + System.getenv("LANG"));
+		System.out.println("Locale.getDefault()=" + Locale.getDefault());
+		System.out.println("Charset.defaultCharset()=" + Charset.defaultCharset());
+		System.out.println("file.encoding=" + System.getProperty("file.encoding"));
+		System.out.println("sun.jnu.encoding=" + System.getProperty("sun.jnu.encoding"));
 		SpringApplication.run(ShopscraperApplication.class, args).close();
 	}
 
