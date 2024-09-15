20d26657efdac354750deca36dcce16fa19cef1d
Andreas Groll <andreas.groll@gmail.com>
2024-09-15 19:33:48 +0200

    feat: jdk 17 with latest graalvm

--- src/main/java/de/networkchallenge/ringana/shopscraper/ShopscraperApplication.java
+++ src/main/java/de/networkchallenge/ringana/shopscraper/ShopscraperApplication.java
@@ -2,15 +2,8 @@ package de.networkchallenge.ringana.shopscraper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.nativex.hint.TypeHint;
-import org.springframework.nativex.hint.TypeHints;
 
 @SpringBootApplication
-@TypeHints(value = {
-		@TypeHint(typeNames = {"de.networkchallenge.ringana.shopscraper.web.model.ShopProduct", "de.networkchallenge.ringana.shopscraper.web.model.ShopProduct$ShopProductBuilder"}),
-		@TypeHint(typeNames = {"de.networkchallenge.ringana.shopscraper.web.model.ShopPrice", "de.networkchallenge.ringana.shopscraper.web.model.ShopPrice$ShopPriceBuilder"}),
-		@TypeHint(typeNames = {"de.networkchallenge.ringana.shopscraper.web.model.ZapiProducts"})
-})
 public class ShopscraperApplication {
 
 	public static void main(String[] args) {
